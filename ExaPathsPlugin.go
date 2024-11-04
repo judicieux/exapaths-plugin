bd66a389234a6fdd7d536a4aa63a1a67ae200808
judicieux <r4nd0mmachine@gmail.com>
2024-11-04 20:30:43 +0100

    Update ExaPathsPlugin.go

--- ExaPathsPlugin.go
+++ ExaPathsPlugin.go
@@ -16,21 +16,22 @@ func (ExaPathsPlugin) GetVersion() (int, int, int) {
 func (ExaPathsPlugin) GetRequests() []l9format.WebPluginRequest {
 	return []l9format.WebPluginRequest{{
 		Method:  "GET",
-		Path:    "/api/geojson?url=file:///etc/hosts",
+		Path:    "/.env.example",
 		Headers: map[string]string{},
 		Body:    []byte(""),
 	}}
 }
 
 func (ExaPathsPlugin) GetName() string {
-	return "ExaPathsPlugin"
+	return "EnvExampleHttpPlugin"
 }
 
 func (ExaPathsPlugin) GetStage() string {
 	return "open"
 }
+
 func (plugin ExaPathsPlugin) Verify(request l9format.WebPluginRequest, response l9format.WebPluginResponse, event *l9format.L9Event, options map[string]string) (hasLeak bool) {
-	// if not checking for request , or not 200 or html, quit
+	// Vérification si la requête correspond, si le code HTTP est 200 et si la taille du corps est raisonnable
 	if !request.EqualAny(plugin.GetRequests()) || response.Response.StatusCode != 200 {
 		return false
 	}
@@ -38,17 +39,14 @@ func (plugin ExaPathsPlugin) Verify(request l9format.WebPluginRequest, response 
 	if len(lowerBody) < 10 {
 		return false
 	}
-	if strings.Contains(lowerBody, "<html") {
-		return false
+	// Vérification du motif "DB_HOST="
+	if strings.Contains(lowerBody, "db_host=") {
+		event.Service.Software.Name = "EnvironmentFile"
+		event.Leak.Type = "config_leak"
+		event.Leak.Severity = "high"
+		event.AddTag("potential-db-credentials")
+		event.Summary = "Found DB_HOST in /.env.example:\n" + string(response.Body)
+		return true
 	}
-	if !strings.Contains(lowerBody, "localhost") && !strings.Contains(lowerBody, "127") {
-		return false
-	}
-	event.Service.Software.Name = "Metabase"
-	event.Leak.Type = "lfi"
-	event.Leak.Severity = "critical"
-	event.AddTag("cve-2021-41277")
-	event.Summary = "Found /etc/hosts through CVE-2021-41277:\n" + string(response.Body)
-	return true
-
+	return false
 }
